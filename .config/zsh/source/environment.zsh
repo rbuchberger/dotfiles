35b15dbe4ec71bf003538377bb46362d9e151469
Robert Buchberger <robert@buchberger.cc>
2020-03-26 12:03:57 +0100

    clean up and fix zsh configs

--- .config/zsh/source/environment.zsh
+++
@@ -1,8 +0,0 @@
-export EDITOR='nvim'
-export BROWSER='qutebrowser'
-export MANWIDTH=80
-export PATH=/home/robert/.config/composer/vendor/bin:$PATH
-# GPG key setting for package builds
-export GPGKEY=8C29272B84A3CC1BD7CBBCF218B7E45B74319F2C
-# Delay by .2 seconds; reduce insert & normal mode switching lag
-export KEYTIMEOUT=2
