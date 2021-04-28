f15d228db35ecf01b9bcea5a17f5e7003ff81920
Robert Buchberger <robert@buchberger.cc>
2021-04-28 10:56:26 +0200

    zsh: add aliases for todo tasks

--- .config/zsh/source/aliases.zsh
+++ .config/zsh/source/aliases.zsh
@@ -8,6 +8,21 @@ alias m="neomutt"
 alias n="newsboat"
 alias c="ikhal"
 
+alias vds="vdirsyncer sync"
+
+alias td="todo"
+alias tdp="todo list Personal"
+alias tdf="todo list Family"
+alias tdw="todo list Work"
+alias tdd="todo done"
+alias tdn="todo new"
+alias tdrm="todo delete"
+alias tde="todo edit"
+alias tdnp="todo new -l Personal"
+alias tdnw="todo new -l Work"
+alias tdnf="todo new -l Family"
+alias tdg="todo | grep"
+
 # alias t="task"
 # alias ta="task add"
 # alias td="task done"
@@ -19,7 +34,7 @@ alias zoom='QT_QPA_PLATFORM="" zoom'
 alias ssh="kitty +kitten ssh"
 
 # Spin down hard drives
-alias shush="sudo hdparm -y /dev/sd{a,b,c,d}"
+# alias shush="sudo hdparm -y /dev/sd{a,b,c,d}"
 
 # Units defaults are dumb.
 alias units="units --verbose --digits 4"
