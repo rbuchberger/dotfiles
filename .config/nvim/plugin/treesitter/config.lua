41db064457d3a9846d8292489a3fef67001e13ca
Robert Buchberger <robert@buchberger.cc>
2021-08-21 18:38:13 +0200

    nvim: configure treesitter incremental selection

--- .config/nvim/plugin/treesitter/config.lua
+++ .config/nvim/plugin/treesitter/config.lua
@@ -6,29 +6,28 @@ require'nvim-treesitter.configs'.setup {
   indent = {
     enable = false
   },
+
   textobjects = {
     select = {
-
-      -- Automatically jump forward to textobj, similar to targets.vim 
       lookahead = true,
-
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
         ["am"] = "@function.outer",
         ["im"] = "@function.inner",
         ["ac"] = "@comment.outer", -- does not work for ruby
         ["ic"] = "@comment.inner", -- does not work for ruby
         ["ib"] = "@block.inner",
         ["ab"] = "@block.outer", -- does not work for ruby
-
-        -- Or you can define your own textobjects like this
-        -- ["iF"] = {
-        --   python = "(function_definition) @function",
-        --   cpp = "(function_definition) @function",
-        --   c = "(function_definition) @function",
-        --   java = "(method_declaration) @function",
-        -- },
       },
     },
   },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    }
+  }
 }
