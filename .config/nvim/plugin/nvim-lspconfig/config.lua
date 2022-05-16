89cd4501bfc2c12962d4b0144e4357b073c9b350
Robert Buchberger <robert@buchberger.cc>
2022-05-16 12:18:23 +0200

    nvim: add rust tooling

--- .config/nvim/plugin/nvim-lspconfig/config.lua
+++ .config/nvim/plugin/nvim-lspconfig/config.lua
@@ -49,6 +49,24 @@ lspconfig.tsserver.setup({
 	end,
 })
 
+lspconfig.rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            assist = {
+                importGranularity = "module",
+                importPrefix = "self",
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
 local servers = { "solargraph", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
