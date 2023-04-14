6b0b27386d43a189915bf9ed980c810263b6a58b
Robert Buchberger <robert@buchberger.cc>
2023-04-14 16:31:45 +0200

    nvim - handle plugin updates

--- .config/nvim/plugin/cmp/config.lua
+++ .config/nvim/plugin/cmp/config.lua
@@ -25,18 +25,6 @@ cmp.setup({
       vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
-  formatting = {
-    format = function(entry, vim_item)
-      vim_item.kind = lspkind.presets.default[vim_item.kind] .. " "
-      vim_item.menu = ({
-            buffer = "[Buffer]",
-            nvim_lsp = "[LSP]",
-            nvim_path = "[Path]",
-            calc = "[Calc]",
-          })[entry.source.name]
-      return vim_item
-    end,
-  },
   mapping = {
         ["<C-p>"] = cmp.mapping(select_prev_item, { "i", "c" }),
         ["<C-n>"] = cmp.mapping(select_next_item, { "i", "c" }),
@@ -80,19 +68,3 @@ if autopairs_ok then
 else
   print("autopairs.completion.cmp not found")
 end
-
--- Completion menu type highlighting
--- vim.cmd [[highlight! link CmpItemAbbrMatchFuzzy Aqua]]
--- vim.cmd [[highlight! link CmpItemKindText Fg]]
--- vim.cmd [[highlight! link CmpItemKindMethod Purple]]
--- vim.cmd [[highlight! link CmpItemKindFunction Purple]]
--- vim.cmd [[highlight! link CmpItemKindConstructor Green]]
--- vim.cmd [[highlight! link CmpItemKindField Aqua]]
--- vim.cmd [[highlight! link CmpItemKindVariable Blue]]
--- vim.cmd [[highlight! link CmpItemKindClass Green]]
--- vim.cmd [[highlight! link CmpItemKindInterface Green]]
--- vim.cmd [[highlight! link CmpItemKindValue Orange]]
--- vim.cmd [[highlight! link CmpItemKindKeyword Keyword]]
--- vim.cmd [[highlight! link CmpItemKindSnippet Red]]
--- vim.cmd [[highlight! link CmpItemKindFile Orange]]
--- vim.cmd [[highlight! link CmpItemKindFolder Orange]]
