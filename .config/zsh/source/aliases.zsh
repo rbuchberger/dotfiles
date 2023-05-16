2a5a2a9d20d875abe57a6db1d536a0a326a08c96
Robert Buchberger <robert@buchberger.cc>
2023-05-16 11:19:41 +0200

    zsh - update aliases
    Remove todo & kitty related ones, add some others

--- .config/zsh/source/aliases.zsh
+++ .config/zsh/source/aliases.zsh
@@ -1,8 +1,10 @@
 # Aliases
-alias icat="kitty +kitten icat"
+# alias icat="kitty +kitten icat"
 alias cat="bat"
 alias be="bundle exec"
 alias bejs="bundle exec jekyll serve --livereload"
+
+alias ls="exa --icons"
 
 alias m="neomutt"
 alias n="newsboat"
@@ -13,39 +15,12 @@ alias zg="lazygit"
 
 alias vds="vdirsyncer sync"
 
-alias td="todo"
-alias tds="todo list --startable --no-reverse"
-alias tdp="todo list --startable --no-reverse Personal"
-alias tdf="todo list --startable --no-reverse Family"
-alias tdw="todo list --startable --no-reverse Work"
-alias tdi="todo list --startable --no-reverse --priority low"
-alias tdd="todo done"
-alias tdn="todo new"
-alias tdrm="todo delete"
-alias tde="todo edit"
-alias tdnp="todo new -l Personal"
-alias tdnw="todo new -l Work"
-alias tdnf="todo new -l Family"
-alias tdg="todo list --grep"
-alias tdml="todo edit --priority low"
-
 alias lazyadm="yadm enter lazygit --work-tree ~/"
 
 alias dim="ddcutil setvcp 10 70"
 alias bright="ddcutil setvcp 10 100"
 
-# alias t="task"
-# alias ta="task add"
-# alias td="task done"
-# alias tm="task mod"
-
 alias zoom='QT_QPA_PLATFORM="" zoom'
-
-# This fixes terminfo problems over ssh
-# alias ssh="kitty +kitten ssh"
-
-# Spin down hard drives
-# alias shush="sudo hdparm -y /dev/sd{a,b,c,d}"
 
 # Units defaults are dumb.
 alias units="units --verbose --digits 4"
@@ -53,3 +28,6 @@ alias units="units --verbose --digits 4"
 alias glances="glances --disable-bg --disable-check-update"
 
 alias record="wf-recorder -p preset=slower -p crf=10 -p tune=animation"
+
+alias copy="wl-copy"
+alias paste="wl-paste"
