6ca974c6ed1b19dc03c44a71e90eb1960e40d84c
Robert Buchberger <robert@buchberger.cc>
2023-12-11 11:23:00 +0100

    nvim - fix splitjoin
    I don't know why they decide to turn off their plugin by defualt for long lines, but i hate it

--- .config/nvim/plugin/treesj/config.lua
+++ .config/nvim/plugin/treesj/config.lua
@@ -2,6 +2,7 @@ require("if_installed")("treesj", function(treesj)
   treesj.setup({
     -- your options here
     use_default_keymaps = false,
+    max_join_length = 99999,
   })
 
   vim.api.nvim_set_keymap("n", "gS", ":TSJSplit<CR>", { silent = true })
