18798074f13dc004e5109cd590f3fd9bcaa7404e
Robert Buchberger <robert@buchberger.cc>
2024-06-15 00:56:26 +0200

    Add ensure_installed to treesitter config

--- .config/nvim/plugin/treesitter/config.lua
+++ .config/nvim/plugin/treesitter/config.lua
@@ -1,37 +1,55 @@
 require("if_installed")("nvim-treesitter.configs", function(treesitter)
-  treesitter.setup({
-    highlight = {
-      enable = true,
-    },
+	treesitter.setup({
+		highlight = {
+			enable = true,
+		},
 
-    indent = {
-      enable = false,
-    },
+		indent = {
+			enable = true,
+		},
 
-    textobjects = {
-      select = {
-        lookahead = true,
-        keymaps = {
-          ["am"] = "@function.outer",
-          ["im"] = "@function.inner",
-          ["ac"] = "@comment.outer",
-          ["ic"] = "@comment.inner",
-          ["ib"] = "@block.inner",
-          ["ab"] = "@block.outer", -- does not work for ruby
-        },
-      },
-    },
+		ensure_installed = {
+			"bash",
+			"c",
+			"css",
+			"go",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"rust",
+			"scss",
+			"typescript",
+			"tsx",
+			"vim",
+			"yaml",
+		},
 
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "<C-n>",
-        scope_incremental = "<C-c>",
-        node_decremental = "<C-m>",
-      },
-    },
+		textobjects = {
+			select = {
+				lookahead = true,
+				keymaps = {
+					["am"] = "@function.outer",
+					["im"] = "@function.inner",
+					["ac"] = "@comment.outer",
+					["ic"] = "@comment.inner",
+					["ib"] = "@block.inner",
+					["ab"] = "@block.outer", -- does not work for ruby
+				},
+			},
+		},
 
-    context_commentstring = { enable = true },
-  })
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn",
+				node_incremental = "<C-n>",
+				scope_incremental = "<C-c>",
+				node_decremental = "<C-m>",
+			},
+		},
+
+		context_commentstring = { enable = true },
+	})
 end)
