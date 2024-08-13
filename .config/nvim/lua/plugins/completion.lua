99f45734b3b9c22518ea06dc2e85d9b9820a83fe
Robert Buchberger <robert@buchberger.cc>
2024-08-13 11:46:04 +0200

    nvim - disable cmp autoselect first item on <cr>
    It was annoying

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -54,7 +54,7 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 				},
 
 				sources = {
